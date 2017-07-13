9373545ac92ab109a977b2b753adbe9554343a78
Jiapengzhang <1315822982@qq.com>
2017-07-13 17:42:06 +0800

    Add files via upload

--- web_docker/lab1.go
+++ web_docker/lab1.go
@@ -1,1 +1,33 @@
+// lab1
+package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type countAdd struct {
+	Count int `json:"count"`
+}
+
+var n = 0
+var c = make(chan int, 10)
+
+func f() {
+	<-c //确保n++正确执行
+	n++
+}
+
+func count_add(w http.ResponseWriter, req *http.Request) {
+	c <- 0
+	go f()
+	output := countAdd{n}
+	var outputJson []byte
+	outputJson, _ = json.Marshal(output)
+	w.Write(outputJson)
+}
+
+func main() {
+	http.HandleFunc("/add", count_add)
+	http.ListenAndServe(":9090", nil)
+}
